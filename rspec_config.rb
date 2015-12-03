98195c368bab6772d090ffc5c79309a0ccebf181
Sergy Nazarevich <aqueelone@gmail.com>
2015-12-03 16:23:31 +0200

    fixed for better back compitable

--- rspec_config.rb
+++ rspec_config.rb
@@ -1,5 +1,5 @@
 RSpec.configure do |c|
   c.fail_fast = true
   c.color = true
-  c.expect_with(:rspec) {|conf| conf.syntax= :should}
+  c.expect_with(:rspec) {|conf| conf.syntax= [:should, :expect]}
 end
